c9b600937a88327a1fc3464e8bd4fb8733f37b2a
Anuj Ghimire <anujghimire08@gmail.com>
2025-11-28 18:22:15 +0545

    add callbackhell.js with asynchronous task execution using callbacks

---
+++ callbackhell.js
@@ -0,0 +1,28 @@
+function task1(cb) {
+  setTimeout(() => {
+    console.log("Task 1 completed");
+    cb();
+  }, 2000);
+}
+function task2(cb) {
+  setTimeout(() => {
+    console.log("Task 2 completed");
+    cb();
+  }, 1000);
+}
+function task3(cb) {
+  setTimeout(() => {
+    console.log("Task 3 completed");
+    cb();
+  }, 2500);
+}
+function task4(cb) {
+  setTimeout(() => {
+    console.log("Task 4 completed");
+    cb();
+  }, 3000);
+}
+
+task1(() => {
+  task2(() => task3(() => task4(() => console.log("All tasks completed"))));
+});
